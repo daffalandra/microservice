530520a42853570ee0109e2194740bd5ece6eef2
Daffa Alandra <mdaf.alandra@gmail.com>
2024-10-21 22:10:27 +0700

    public-api commit, initial public-api commit

---
+++ public-api/src/test/java/com/exercise/publicapi/PublicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.publicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
